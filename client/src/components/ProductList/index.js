cf4379754295ef1362617fcf05ff7cbb0d38441c
Dzarbis <dzarbis@gmail.com>
2021-07-25 22:38:13 -0400

    22.2

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,10 +2,10 @@ import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import spinner from '../../assets/spinner.gif';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { QUERY_PRODUCTS } from '../../utils/queries';
+import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
